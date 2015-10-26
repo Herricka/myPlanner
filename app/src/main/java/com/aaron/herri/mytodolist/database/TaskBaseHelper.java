71e45e32c54351a4682390ad2366961c918372d7
Aaron Herrick <herricka1@hotmail.com>
2015-10-26 18:07:46 -0500

    update 10/26

--- app/src/main/java/com/aaron/herri/mytodolist/database/TaskBaseHelper.java
+++ app/src/main/java/com/aaron/herri/mytodolist/database/TaskBaseHelper.java
@@ -15,27 +15,11 @@ public class TaskBaseHelper extends SQLiteOpenHelper {
     // Database Name
     private static final String DATABASE_NAME = "plannerBase.db";
 
-    // Tasks Table Columns names
-    private static final String TASK_ID = "id";
-    private static final String TASK_TITLE = "title";
-
-    // Bills Table Columns names
-    private static final String BILL_ID = "id";
-    private static final String BILL_TiTLE = "title";
-
     public TaskBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        String CREATE_TASKS_TABLE = "CREATE TABLE" + DATABASE_NAME + "("
-                + TASK_ID + " INTEGER PRIMARY KEY," + TASK_TITLE + " TEXT,"
-
-    }
-
-
-    /*@Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TaskTable.NAME + "(" +
         " _id integer primary key autoincrement, " +
@@ -48,7 +32,7 @@ public class TaskBaseHelper extends SQLiteOpenHelper {
         ")"
         );
     }
-    */
+
 
 
     @Override
